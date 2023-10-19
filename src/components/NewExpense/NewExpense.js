294d2822d0ef52c6a577f125cd46b07d95464121
Parimal Donga <parimaldonga00@gmail.com>
2023-10-19 09:36:12 -0400

    Lifting State Up

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,17 @@ import "./NewExpense.css";
 import "./ExpenseForm";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) =>{
+    const expenseData = {
+      ...enteredExpenseData,
+      id : Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
     </div>
   );
 };
