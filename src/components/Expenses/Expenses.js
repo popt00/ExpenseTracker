6517679f57688b20fba9ee21845d942171d8e049
Parimal Donga <parimaldonga00@gmail.com>
2023-10-20 10:49:17 -0400

    added expenses No found expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,40 +1,25 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
-
+import ExpenseList from "./ExpenseList";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  
+
   const onSaveDateHandling = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpense = props.items.filter(
-    (item) => {return item.date.getFullYear().toString() === filteredYear;}
-  );
-  // console.log(props.items);
-  // console.log(props.items.filter(
-  //   (item) => {return item.date.getFullYear().to === "2021";}
-  // ));
-  // console.log(props.items.filter(
-  //   (item) => {return item.date.getFullYear() === "2022";}
-  // ));
-
+  const filteredExpense = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
+  console.log(filteredExpense);
   return (
     <div>
-      <ExpenseFilter onSaveDate={onSaveDateHandling} />
       <Card className="expenses">
-        {filteredExpense.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpenseFilter onSaveDate={onSaveDateHandling} />
+        <ExpenseList items = {filteredExpense}/>
       </Card>
     </div>
   );
